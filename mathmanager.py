465952a5325089d61d795ed1db0c574d2949fa5a
leomosley <leo.mosley2@gmail.com>
2025-03-25 11:28:42 +0000

    completed

--- mathmanager.py
+++ mathmanager.py
@@ -1,10 +1,47 @@
 class mathmanager:
 
 	def add(self, a, b):
-			return a+b
+		return a+b
 
 	def subtract(self, a, b):
-			return a-b
+		return a-b
 
 	def multiply(self, a, b):
-			return a*b
+		return a*b
+	
+	def monthly_intrest(self, amount, period):
+		if period == 1:
+			rate = 1.038
+		elif period == 2:
+			rate = 1.036
+		else:
+			raise ValueError("Invalid period, must be 1 or 2 years")
+
+		return amount * (rate ** period)
+
+	def tax(self, income):
+		if income <= 12570:
+			rate = 0
+		elif income <= 50270:
+			rate = 0.2
+		elif income <= 125140:
+			rate = 0.4
+		else:
+			rate = 0.45
+		
+		return income * rate
+
+	def degree_classification(self, grades):
+		grades.remove(min(grades))
+		average = sum(grades) / len(grades)
+
+		if average < 40:
+			return "Fail"
+		elif average < 50:
+			return "Third"
+		elif average < 60:
+			return "Second"
+		elif average < 70:
+			return "Upper Second"
+		else:
+			return "First"
--- mathmanagertest.py
+++ mathmanagertest.py
@@ -13,3 +13,20 @@ class mathmanagertest(unittest.TestCase):
 	def testMultiply(self):
 		math = mathmanager()
 		self.assertEqual(math.multiply(0, 3), 0)
+
+	def testMonthlyIntrest(self):
+		math = mathmanager()
+		self.assertEqual(math.monthly_intrest(1000, 1), 1038.00)
+		self.assertEqual(math.monthly_intrest(1000, 2), 1073.296)
+	
+	def testTax(self):
+		math = mathmanager()
+		self.assertEqual(math.tax(12570), 0)
+		self.assertEqual(math.tax(50270), 10054)
+		self.assertEqual(math.tax(125140), 50056)
+	
+	def testDegreeClassification(self):
+		math = mathmanager()
+		self.assertEqual(math.degree_classification([40, 50, 60, 70, 80]), "Upper Second")
+		self.assertEqual(math.degree_classification([40, 50, 60, 70]), "Upper Second")
+		self.assertEqual(math.degree_classification([40, 50, 60]), "Second")
